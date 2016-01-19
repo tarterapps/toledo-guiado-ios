87e28be91b4ea065bd751ba43767cbf6015b3a5d
cristobal <chermidap@gmail.com>
2016-01-19 20:35:54 +0100

    initial import

---
+++ ToledoGuiadoV2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ToledoGuiadoV2
+//
+//  Created by Cristobal Hermida Portales on 15/1/16.
+//  Copyright © 2016 Cristobal Hermida Portales. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
